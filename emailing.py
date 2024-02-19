de9b15909207d5450d9c4fd9d06deb7ef5267112
chad-oneal <coneal@regscale.com>
2024-02-19 13:36:50 -0500

    Finished application and added code comments

--- emailing.py
+++ emailing.py
@@ -1,2 +1,41 @@
-def send_email():
-    print('Email was sent!')
+import smtplib  # Importing the SMTP library for sending emails
+import imghdr  # Importing the imghdr library to determine the image type
+from email.message import EmailMessage  # Importing the EmailMessage class for email composition
+
+PASSWORD = 'stdzzmsrgfzhccfv'  # Sender's email password (ensure secure handling)
+SENDER = 'chadoneal3@gmail.com'  # Sender's email address
+RECEIVER = 'chadoneal3@gmail.com'  # Receiver's email address
+
+def send_email(image_path):
+    """
+    Function to send an email with an image attachment.
+    :param image_path: Path to the image file to be attached.
+    """
+    print('Sending email...')  # Informing about the email sending process
+
+    # Creating an instance of EmailMessage for composing the email
+    email_message = EmailMessage()
+    email_message['Subject'] = 'Webcam Detection'  # Setting the email subject
+    email_message.set_content('New Webcam Detection')  # Setting the email content
+
+    # Opening the image file in binary mode and reading its content
+    with open(image_path, 'rb') as file:
+        content = file.read()
+
+    # Adding the image file as an attachment to the email message
+    email_message.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+
+    # Establishing a connection to Gmail's SMTP server
+    gmail = smtplib.SMTP('smtp.gmail.com', 587)
+    gmail.ehlo()  # Starting the SMTP connection
+    gmail.starttls()  # Initiating TLS encryption for secure communication
+    gmail.login(SENDER, PASSWORD)  # Logging into the sender's email account
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())  # Sending the email message
+    gmail.quit()  # Quitting the SMTP server connection
+    print('Email has been sent')  # Notifying about the successful email sending
+
+
+if __name__ == '__main__':
+    # Calling the send_email function with a sample image path
+    send_email(image_path='images/19.png')
+
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 import time
+import glob
+import os
 from emailing import send_email
+from threading import Thread
 
 # Open the video capture from the default camera (camera index 0)
 video = cv2.VideoCapture(0)
@@ -11,32 +14,38 @@ time.sleep(1)
 # Initialize a variable to store the first frame for reference
 first_frame = None
 status_list = []
+count = 1
+
+def clean_folder():
+    """
+    Function to clean the 'images' folder by removing all PNG files.
+    """
+    print('Cleaning folder....')
+    images = glob.glob('images/*.png')
+    for image in images:
+        os.remove(image)
+    print('Folder has been cleaned')
+
 
 # Infinite loop to capture and process frames from the webcam
 while True:
     status = 0
     # Read a frame from the webcam
     check, frame = video.read()
-
     # Convert the frame to grayscale
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # Apply Gaussian blur to the grayscale frame
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
-
     # If it's the first frame, store it as the reference frame
     if first_frame is None:
         first_frame = gray_frame_gau
 
     # Calculate the absolute difference between the current frame and the reference frame
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
-
     # Apply a binary threshold to the difference frame
     thresh_frame = cv2.threshold(delta_frame, 55, 255, cv2.THRESH_BINARY)[1]
-
     # Perform dilation on the threshold frame
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-
     # Display the dilated frame with moving object outlines
     cv2.imshow('Webcam Video', dil_frame)
 
@@ -54,13 +63,31 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob('images/*.png')
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
+    # Append the current status (0 or 1) to the status list
     status_list.append(status)
+    # Keep only the last two elements in the status list
     status_list = status_list[-2:]
 
+    # Check if the status transitioned from 1 (object detected) to 0 (no object detected)
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        # Create a new thread to send an email with the image containing the detected object
+        email_thread = Thread(target=send_email, args=(image_with_object,))
+        # Set the email thread as a daemon thread, which will terminate if the main program exits
+        email_thread.daemon = True
+        # Create a new thread to clean the images folder
+        clean_thread = Thread(target=clean_folder)
+        # Set the clean folder thread as a daemon thread
+        clean_thread.daemon = True
+        # Start the email thread to send the email with the image
+        email_thread.start()
 
+    # Print the current status list (useful for debugging or monitoring)
     print(status_list)
 
     # Display the original frame with object detection rectangles
@@ -73,6 +100,10 @@ while True:
 
 # Release the video capture object
 video.release()
+
+# Start the thread to clean the folder
+clean_thread.start()
+
 
 
 
